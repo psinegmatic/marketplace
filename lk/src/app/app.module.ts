e4ff190ac34735fbf46586fa7b0a40936864abbc
aleksey <kovalchuk.aleksey.s@gmail.com>
2017-10-04 10:15:08 +0300

    Добавлены компоненты, изменены настройки порта для проекта lk

--- lk/src/app/app.module.ts
+++ lk/src/app/app.module.ts
@@ -9,14 +9,28 @@ import { NgbModule } from  '@ng-bootstrap/ng-bootstrap';
 
 /* Import components */
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PriceListsComponent } from './components/price-lists/price-lists.component';
+import { PriceListEditComponent } from './components/price-list-edit/price-list-edit.component';
 
 /* Routes */
 const appRoutes: Routes = [
-  //{path: '', component: HomeComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: '', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
   declarations: [
+    AppComponent,
+    DashboardComponent,
+    SigninComponent,
+    SignupComponent,
+    PriceListsComponent,
+    PriceListEditComponent
   ],
   imports: [
     BrowserModule,
